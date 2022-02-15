efeac9103c17151da8318afd53e90005b071fee1
Valerii <valerii.pihalov@truepill.com>
2022-02-15 19:26:12 +0200

    feat(mon-14): eslint move to workspace root

--- .eslintrc.ts
+++ .eslintrc.ts
@@ -1,60 +1,24 @@
 module.exports = {
-  root: true,
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
     sourceType: 'module',
   },
-  plugins: [
-    '@typescript-eslint/eslint-plugin',
-    'prettier',
-    'simple-import-sort',
-    'unused-imports',
-  ],
+  plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
-    'prettier',
-    'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:prettier/recommended',
   ],
+  root: true,
   env: {
     node: true,
     jest: true,
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
-    'unused-imports/no-unused-imports': 'error',
-    'max-len': [
-      1,
-      {
-        code: 120,
-        ignoreStrings: true,
-        ignoreTemplateLiterals: true,
-        ignoreComments: true,
-      },
-    ],
-    'no-console': 1,
-    'prettier/prettier': 2,
-    'import/prefer-default-export': 0,
-    'simple-import-sort/imports': 1,
-    '@typescript-eslint/explicit-module-boundary-types': 0,
-    '@typescript-eslint/naming-convention': [
-      1,
-      {
-        selector: 'interface',
-        format: ['PascalCase'],
-        custom: {
-          regex: '^I[A-Z]',
-          match: true,
-        },
-      },
-    ],
-    '@typescript-eslint/no-empty-interface': [
-      1,
-      {
-        allowSingleExtends: true,
-      },
-    ],
-    '@typescript-eslint/no-explicit-any': 2,
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
   },
 };
